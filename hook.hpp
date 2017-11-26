a4968af0eb393781f8c18abf2161848ef9bd7b15
Yuchen Wang <yulon@me.com>
2017-11-27 01:45:50 +0800

    Added hook

---
+++ hook.hpp
@@ -0,0 +1,83 @@
+#ifndef _TMD_HOOK_HPP
+#define _TMD_HOOK_HPP
+
+#include <MinHook.h>
+
+#include <functional>
+#include <vector>
+#include <atomic>
+
+namespace tmd {
+	template <typename T>
+	class hook {
+		public:
+			using target_type = T;
+
+			constexpr hook() : _t(nullptr), _o(nullptr) {}
+
+			hook(T target, T detour) :
+				_t(reinterpret_cast<LPVOID>(target))
+			{
+				if (++_count == 1) {
+					MH_Initialize();
+				}
+				MH_CreateHook(reinterpret_cast<LPVOID>(target), reinterpret_cast<LPVOID>(detour), &_o);
+				MH_EnableHook(reinterpret_cast<LPVOID>(target));
+			}
+
+			hook(const hook<T> &) = delete;
+
+			hook<T>& operator=(const hook<T> &) = delete;
+
+			hook(hook<T> &&src) : _t(src._t), _o(src._o) { src._t = nullptr; }
+
+			hook<T> &operator=(hook<T> &&src) {
+				unhook();
+
+				if (src._t) {
+					_t = src._t;
+					_o = src._o;
+
+					src._t = nullptr;
+				}
+
+				return *this;
+			}
+
+			~hook() {
+				unhook();
+			}
+
+			template <typename... A>
+			std::invoke_result_t<T, A...> original_fn(A&&... a) const {
+				return std::invoke(reinterpret_cast<T>(_t), std::forward<A>(a)...);
+			}
+
+			void unhook() {
+				if (_t) {
+					MH_DisableHook(_t);
+					MH_RemoveHook(_t);
+					_t = nullptr;
+					if (!--_count) {
+						MH_Uninitialize();
+					}
+				}
+			}
+
+			operator bool() const {
+				return _t;
+			}
+
+			template <typename... A>
+			void assign(A&&... a) {
+				*this = hook<T>(std::forward<A>(a)...);
+			}
+
+		private:
+			LPVOID _t, _o;
+
+			static std::atomic<size_t> _count;
+	};
+}
+
+#endif
