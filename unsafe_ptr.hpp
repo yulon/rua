053392aec5004257e7731d3241dabe87752bd2ae
Yulon <yulon@me.com>
2018-03-13 04:23:22 +0800

    Improved _t2u & _u2t

--- unsafe_ptr.hpp
+++ unsafe_ptr.hpp
@@ -22,11 +22,15 @@ namespace rua {
 			constexpr unsafe_ptr(uintptr_t src) : _val(src) {}
 
 			template <typename T>
-			RUA_CONSTEXPR_14 unsafe_ptr(T &&src) : _val(
-				_t2u<
+			RUA_CONSTEXPR_14 unsafe_ptr(T &&src) : _val(_t2u(
+				std::forward<T>(src),
+				std::integral_constant<bool,
 					std::is_integral<typename std::remove_reference<T>::type>::value ||
-					(std::is_class<typename std::remove_reference<T>::type>::value && std::is_convertible<T, uintptr_t>::value)
-				>::fn(std::forward<T>(src)
+					(
+						std::is_class<typename std::remove_reference<T>::type>::value &&
+						std::is_convertible<T, uintptr_t>::value
+					)
+				>()
 			)) {}
 
 			uintptr_t &value() {
@@ -47,13 +51,17 @@ namespace rua {
 
 			template <typename T>
 			T to() const {
-				return _u2t<
-					T,
-					std::is_pointer<typename std::remove_reference<T>::type>::value ||
-					std::is_member_pointer<typename std::remove_reference<T>::type>::value,
-					std::is_integral<typename std::remove_reference<T>::type>::value ||
-					(std::is_class<typename std::remove_reference<T>::type>::value && std::is_convertible<uintptr_t, T>::value)
-				>::fn(_val);
+				return _u2t<T>(
+					_val,
+					std::integral_constant<bool,
+						std::is_pointer<typename std::remove_reference<T>::type>::value ||
+						std::is_member_pointer<typename std::remove_reference<T>::type>::value
+					>(),
+					std::integral_constant<bool,
+						std::is_integral<typename std::remove_reference<T>::type>::value ||
+						(std::is_class<typename std::remove_reference<T>::type>::value && std::is_convertible<uintptr_t, T>::value)
+					>()
+				);
 			}
 
 			template <typename T>
@@ -64,69 +72,51 @@ namespace rua {
 		private:
 			uintptr_t _val;
 
-			template <bool IS_DRCT>
-			struct _t2u;
+			template <typename T>
+			static constexpr uintptr_t _t2u(T &&src, std::true_type /*is_drct*/) {
+				return src;
+			}
 
-			template <typename T, bool IS_PTR, bool IS_DRCT>
-			struct _u2t;
-	};
+			template <typename T>
+			static RUA_CONSTEXPR_14 uintptr_t _t2u(T &&src, std::false_type /*is_drct*/) {
+				using src_t = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
-	template <>
-	struct unsafe_ptr::_t2u<true> {
-		template <typename T>
-		static constexpr uintptr_t fn(T &&src) {
-			return src;
-		}
-	};
+				RUA_STATIC_ASSERT(std::is_trivially_copyable<src_t>::value);
 
-	template <>
-	struct unsafe_ptr::_t2u<false> {
-		template <typename T>
-		static RUA_CONSTEXPR_14 uintptr_t fn(T &&src) {
-			using src_t = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+				if RUA_CONSTEXPR_IF (sizeof(src_t) < sizeof(uintptr_t)) {
+					uintptr_t val = 0;
+					*reinterpret_cast<src_t *>(&val) = src;
+					return val;
+				} else {
+					return *reinterpret_cast<const uintptr_t *>(&src);
+				}
+			}
 
-			RUA_STATIC_ASSERT(std::is_trivially_copyable<src_t>::value);
+			template <typename T, typename IS_DRCT>
+			static constexpr T _u2t(const uintptr_t &val, std::true_type /*is_ptr*/, IS_DRCT &&) {
+				return reinterpret_cast<T>(val);
+			}
 
-			if RUA_CONSTEXPR_IF (sizeof(src_t) < sizeof(uintptr_t)) {
-				uintptr_t val = 0;
-				*reinterpret_cast<src_t *>(&val) = src;
+			template <typename T>
+			static constexpr T _u2t(const uintptr_t &val, std::false_type /*is_ptr*/, std::true_type /*is_drct*/) {
 				return val;
-			} else {
-				return *reinterpret_cast<const uintptr_t *>(&src);
 			}
-		}
-	};
 
-	template <typename T, bool IS_DRCT>
-	struct unsafe_ptr::_u2t<T, true, IS_DRCT> {
-		static constexpr T fn(const uintptr_t &val) {
-			return reinterpret_cast<T>(val);
-		}
-	};
+			template <typename T>
+			static RUA_CONSTEXPR_14 T _u2t(const uintptr_t &val, std::false_type /*is_ptr*/, std::false_type /*is_drct*/) {
+				using dest_t = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
-	template <typename T>
-	struct unsafe_ptr::_u2t<T, false, true> {
-		static constexpr T fn(const uintptr_t &val) {
-			return val;
-		}
-	};
+				RUA_STATIC_ASSERT(std::is_trivially_copyable<dest_t>::value);
 
-	template <typename T>
-	struct unsafe_ptr::_u2t<T, false, false> {
-		static RUA_CONSTEXPR_14 T fn(const uintptr_t &val) {
-			using dest_t = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
-
-			RUA_STATIC_ASSERT(std::is_trivially_copyable<dest_t>::value);
-
-			if RUA_CONSTEXPR_IF (sizeof(uintptr_t) < sizeof(dest_t)) {
-				uint8_t r[sizeof(dest_t)];
-				memset(reinterpret_cast<void *>(&r), 0, sizeof(dest_t));
-				*reinterpret_cast<uintptr_t *>(&r) = val;
-				return *reinterpret_cast<const dest_t *>(&r);
-			} else {
-				return *reinterpret_cast<const dest_t *>(&val);
+				if RUA_CONSTEXPR_IF (sizeof(uintptr_t) < sizeof(dest_t)) {
+					uint8_t r[sizeof(dest_t)];
+					memset(reinterpret_cast<void *>(&r), 0, sizeof(dest_t));
+					*reinterpret_cast<uintptr_t *>(&r) = val;
+					return *reinterpret_cast<const dest_t *>(&r);
+				} else {
+					return *reinterpret_cast<const dest_t *>(&val);
+				}
 			}
-		}
 	};
 }
 
