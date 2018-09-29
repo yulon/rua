202c360763d065764840cdb7a1ef19417d037f0b
Yulon <yulon@me.com>
2018-09-29 13:22:42 +0800

    Added line_reader{}

--- include/rua/str.hpp
+++ include/rua/str.hpp
@@ -1,5 +1,5 @@
-#ifndef _RUA_STRENC_HPP
-#define _RUA_STRENC_HPP
+#ifndef _RUA_STR_HPP
+#define _RUA_STR_HPP
 
 #include "bin.hpp"
 #include "io/util.hpp"
@@ -80,7 +80,9 @@ namespace rua {
 
 		class l_to_u8_reader : public virtual io::reader {
 			public:
-				l_to_u8_reader(io::reader &l_reader) : _lr(&l_reader) {}
+				l_to_u8_reader() : _lr(nullptr), _data_sz(0) {}
+
+				l_to_u8_reader(io::reader &l_reader) : _lr(&l_reader), _data_sz(0) {}
 
 				virtual ~l_to_u8_reader() = default;
 
@@ -123,6 +125,8 @@ namespace rua {
 
 		class u8_to_l_writer : public virtual io::writer {
 			public:
+				u8_to_l_writer() : _lw(nullptr) {}
+
 				u8_to_l_writer(io::writer &l_writer) : _lw(&l_writer) {}
 
 				virtual ~u8_to_l_writer() = default;
@@ -148,6 +152,80 @@ namespace rua {
 		#define RUA_L_TO_U8(str) str
 		#define RUA_U8_TO_L(u8_str) u8_str
 	#endif
+
+	////////////////////////////////////////////////////////////////////////////
+
+	static constexpr const char *lf = "\n";
+
+	static constexpr const char *crlf = "\r\n";
+
+	static constexpr const char *cr = "\r";
+
+	#ifdef _WIN32
+		static constexpr const char *line_end = crlf;
+	#elif RUA_DARWIN
+		static constexpr const char *line_end = cr;
+	#else
+		static constexpr const char *line_end = lf;
+	#endif
+
+	class line_reader {
+		public:
+			line_reader() : _r(nullptr), _1st_chr_is_cr(false) {}
+
+			line_reader(io::reader &r) : _r(&r), _1st_chr_is_cr(false) {}
+
+			std::string read_line(size_t buf_sz = 256) {
+				for (;;) {
+					_buf.resize(buf_sz);
+					auto sz = _r->read(_buf);
+					if (!sz) {
+						_r = nullptr;
+						_1st_chr_is_cr = false;
+						return std::move(_cache);
+					}
+					if (_1st_chr_is_cr) {
+						_1st_chr_is_cr = false;
+						if (_buf[0] == static_cast<uint8_t>('\n')) {
+							_buf.slice_self(1);
+						}
+					}
+					for (size_t i = 0; i < sz; ++i) {
+						if (_buf[i] == static_cast<uint8_t>('\r')) {
+							_cache += std::string(_buf.base().to<const char *>(), i);
+							if (i == sz - 1) {
+								_1st_chr_is_cr = true;
+								return std::move(_cache);
+							}
+
+							auto end = i + 1;
+							if (_buf[i + 1] == static_cast<uint8_t>('\n')) {
+								_1st_chr_is_cr = true;
+								end = i + 1;
+							} else {
+								end = i;
+							}
+
+							std::string r(std::move(_cache));
+							_cache = std::string((_buf.base() + end).to<const char *>(), _buf.size() - end);
+							return r;
+
+						} else if (_buf[i] == static_cast<uint8_t>('\n')) {
+							_cache += std::string(_buf.base().to<const char *>(), i);
+							return std::move(_cache);
+						}
+					}
+					_cache += std::string(_buf.base(), sz);
+				}
+				return "";
+			}
+
+		private:
+			io::reader *_r;
+			bool _1st_chr_is_cr;
+			bin _buf;
+			std::string _cache;
+	};
 }
 
 #endif
