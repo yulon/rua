fb66dca6a7d7e7cc10727f7c2f04a022e300a82f
Yulon <yulon@me.com>
2019-08-09 15:23:09 +0800

    Fixed

--- include/rua/bit.hpp
+++ include/rua/bit.hpp
@@ -50,12 +50,15 @@ RUA_FORCE_INLINE void bit_aligned_set(void *ptr, ptrdiff_t ix, const T &val) {
 	return bit_set<T>(ptr, ix * sizeof(T), val);
 }
 
-template <class To, class From>
-RUA_FORCE_INLINE typename std::enable_if<
-	(sizeof(To) == sizeof(From)) && std::is_trivially_copyable<From>::value &&
-		std::is_trivial<To>::value,
-	To>::type
-bit_cast(const From &src) noexcept {
+template <
+	typename To,
+	typename From,
+	typename = typename std::enable_if<
+		(sizeof(To) == sizeof(From)) &&
+			std::is_trivially_copyable<From>::value &&
+			std::is_trivial<To>::value,
+		To>::type>
+RUA_FORCE_INLINE To bit_cast(const From &src) {
 	return bit_get<To>(&src);
 }
 
