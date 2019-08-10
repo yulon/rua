d7a5b63dc93b89e21f1594f18715ee1891b64b1b
Yulon <yulon@me.com>
2019-08-10 09:22:19 +0800

    Changed void * to T * for bit functions

--- include/rua/bit.hpp
+++ include/rua/bit.hpp
@@ -9,45 +9,42 @@
 
 namespace rua {
 
-template <typename T>
+template <typename T, typename P>
 RUA_FORCE_INLINE typename std::enable_if<std::is_trivial<T>::value, T>::type
-bit_get(const void *ptr, ptrdiff_t offset = 0) {
+bit_get(P *ptr) {
 	typename std::aligned_storage<sizeof(T), alignof(T)>::type sto;
-	memcpy(
-		&sto,
-		reinterpret_cast<const void *>(
-			reinterpret_cast<uintptr_t>(ptr) + offset),
-		sizeof(T));
+	memcpy(&sto, reinterpret_cast<const void *>(ptr), sizeof(T));
 	return reinterpret_cast<T &>(sto);
 }
 
-template <typename T>
+template <typename T, typename P>
 RUA_FORCE_INLINE typename std::enable_if<std::is_trivial<T>::value, T>::type
-bit_aligned_get(const void *ptr, ptrdiff_t ix = 0) {
-	return bit_get<T>(ptr, ix * sizeof(T));
+bit_get(P *ptr, ptrdiff_t offset) {
+	return bit_get<T>(reinterpret_cast<const uint8_t *>(ptr) + offset);
 }
 
-template <
-	typename T,
-	typename = typename std::enable_if<std::is_trivial<T>::value, T>::type>
-RUA_FORCE_INLINE void bit_set(void *ptr, const T &val) {
-	memcpy(ptr, &val, sizeof(T));
+template <typename T, typename P>
+RUA_FORCE_INLINE typename std::enable_if<std::is_trivial<T>::value, T>::type
+bit_aligned_get(P *ptr, ptrdiff_t ix) {
+	return bit_get<T>(reinterpret_cast<const T *>(ptr) + ix);
 }
 
-template <
-	typename T,
-	typename = typename std::enable_if<std::is_trivial<T>::value, T>::type>
-RUA_FORCE_INLINE void bit_set(void *ptr, ptrdiff_t offset, const T &val) {
-	bit_set<T>(
-		reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(ptr) + offset),
-		val);
+template <typename T, typename P>
+RUA_FORCE_INLINE typename std::enable_if<std::is_trivial<T>::value>::type
+bit_set(P *ptr, const T &val) {
+	memcpy(reinterpret_cast<void *>(ptr), &val, sizeof(T));
 }
 
-template <
-	typename T,
-	typename = typename std::enable_if<std::is_trivial<T>::value, T>::type>
-RUA_FORCE_INLINE void bit_aligned_set(void *ptr, ptrdiff_t ix, const T &val) {
-	return bit_set<T>(ptr, ix * sizeof(T), val);
+template <typename T, typename P>
+RUA_FORCE_INLINE typename std::enable_if<std::is_trivial<T>::value>::type
+bit_set(P *ptr, ptrdiff_t offset, const T &val) {
+	bit_set<T>(reinterpret_cast<uint8_t *>(ptr) + offset, val);
+}
+
+template <typename T, typename P>
+RUA_FORCE_INLINE typename std::enable_if<std::is_trivial<T>::value>::type
+bit_aligned_set(P *ptr, ptrdiff_t ix, const T &val) {
+	return bit_set<T>(reinterpret_cast<T *>(ptr) + ix, val);
 }
 
 template <typename To, typename From>
