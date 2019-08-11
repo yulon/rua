833d8b99659240ad0507216709d85024867ad803
Yulon <yulon@me.com>
2019-08-11 20:59:16 +0800

    Revised

--- include/rua/string/string_view.hpp
+++ include/rua/string/string_view.hpp
@@ -23,62 +23,52 @@ namespace rua {
 template <typename CharT, typename Traits = std::char_traits<CharT> >
 class basic_string_view {
 public:
-	constexpr basic_string_view() : _s(nullptr), _c(0), _max_c(0) {}
+	constexpr basic_string_view() : _s(nullptr), _c(0) {}
 
 	constexpr basic_string_view(std::nullptr_t) : basic_string_view() {}
 
+#ifdef RUA_CONSTEXPR_14_SUPPORTED
+	constexpr basic_string_view(const CharT *s, size_t count) :
+		_s(s),
+		_c(count) {}
+	constexpr basic_string_view(const CharT *s) : _s(s), _c(_strlen(s)) {}
+#else
 	static constexpr auto _nullsz = static_cast<size_t>(-3);
-
 	constexpr basic_string_view(const CharT *s, size_t count = _nullsz) :
 		_s(s),
-		_c(count),
-		_max_c(count) {}
+		_c(count) {}
+#endif
 
 	basic_string_view(const std::basic_string<CharT, Traits> &s) :
 		basic_string_view(s.c_str(), s.size()) {}
 
-	basic_string_view(const basic_string_view &src) :
-		_s(src._s),
-		_c(src._c),
-		_max_c(src._max_c) {}
-
-	RUA_OVERLOAD_ASSIGNMENT_L(basic_string_view)
-
-	size_t size() {
-		if (_max_c == _nullsz) {
-			_c = _calc_len();
-			_max_c = _c;
+	RUA_CONSTEXPR_14 size_t size() {
+#ifndef RUA_CONSTEXPR_14_SUPPORTED
+		if (_c == _nullsz) {
+			_c = _strlen(_s);
 		}
+#endif
 		return _c;
 	}
 
-	size_t size() const {
-		if (_max_c == _nullsz) {
-			return _calc_len();
+	RUA_CONSTEXPR_14 size_t size() const {
+#ifndef RUA_CONSTEXPR_14_SUPPORTED
+		if (_c == _nullsz) {
+			return _strlen(_s);
 		}
+#endif
 		return _c;
 	}
 
-	size_t max_size() {
-		if (_max_c == _nullsz) {
-			_c = _calc_len();
-			_max_c = _c;
-		}
-		return _max_c;
+	constexpr size_t max_size() const {
+		return static_cast<size_t>(-1);
 	}
 
-	size_t max_size() const {
-		if (_max_c == _nullsz) {
-			return _calc_len();
-		}
-		return _c;
-	}
-
-	size_t length() {
+	RUA_CONSTEXPR_14 size_t length() {
 		return size();
 	}
 
-	size_t length() const {
+	RUA_CONSTEXPR_14 size_t length() const {
 		return size();
 	}
 
@@ -86,33 +76,33 @@ public:
 		return _s;
 	}
 
-	constexpr bool empty() const {
+	RUA_CONSTEXPR_14 bool empty() const {
 		return !size();
 	}
 
-	const CharT &operator[](ptrdiff_t ix) const {
+	constexpr const CharT &operator[](ptrdiff_t ix) const {
 		return _s[ix];
 	}
 
-	const CharT *begin() const {
+	constexpr const CharT *begin() const {
 		return data();
 	}
 
-	const CharT *cbegin() const {
+	constexpr const CharT *cbegin() const {
 		return data();
 	}
 
-	const CharT *end() const {
+	constexpr const CharT *end() const {
 		return begin() + size();
 	}
 
-	const CharT *cend() const {
+	constexpr const CharT *cend() const {
 		return cbegin() + size();
 	}
 
 	static constexpr size_t npos = static_cast<size_t>(-1);
 
-	size_t find(basic_string_view v, size_t pos = 0) const {
+	RUA_CONSTEXPR_14 size_t find(basic_string_view v, size_t pos = 0) const {
 		auto vsz = v.size();
 		auto end_ix = size() - vsz + 1;
 		for (size_t i = pos; i < end_ix; ++i) {
@@ -126,15 +116,16 @@ public:
 		return npos;
 	}
 
-	size_t find(CharT ch, size_t pos = 0) const {
-		return find(basic_string_view(ch, 1), pos);
+	RUA_CONSTEXPR_14 size_t find(CharT ch, size_t pos = 0) const {
+		return find(basic_string_view(&ch, 1), pos);
 	}
 
-	size_t find(const CharT *s, size_t pos, size_t count) const {
+	RUA_CONSTEXPR_14 size_t
+	find(const CharT *s, size_t pos, size_t count) const {
 		return find(basic_string_view(s, count), pos);
 	}
 
-	bool operator==(basic_string_view v) const {
+	RUA_CONSTEXPR_14 bool operator==(basic_string_view v) const {
 		auto sz = size();
 		if (size() != v.size()) {
 			return false;
@@ -147,18 +138,17 @@ public:
 		return true;
 	}
 
-	bool operator!=(basic_string_view v) const {
+	RUA_CONSTEXPR_14 bool operator!=(basic_string_view v) const {
 		return !(*this == v);
 	}
 
 private:
 	const CharT *_s;
 	size_t _c;
-	size_t _max_c;
 
-	size_t _calc_len() const {
+	static RUA_CONSTEXPR_14 size_t _strlen(const CharT *c_str) {
 		size_t i = 0;
-		while (_s[i] != 0) {
+		while (c_str[i] != 0) {
 			++i;
 		}
 		return i;
