403512cc6f9a157a4c69d2fcb4b8425b57abc4c4
Yulon <yulon@me.com>
2019-08-25 17:51:16 +0800

    Fixed

--- include/rua/switch_executable_section.h
+++ include/rua/switch_executable_section.h
@@ -1,9 +1,13 @@
 #ifndef RUA_EXECUTABLE
 
-#ifdef _MSC_VER
-#define RUA_EXECUTABLE
+#ifdef __GNUC__
+#if defined(__APPLE__) && defined(__MACH__)
+#define RUA_EXECUTABLE __attribute__((section("__TEXT,__rua_text")))
 #else
 #define RUA_EXECUTABLE __attribute__((section(".text")))
+#endif
+#else
+#define RUA_EXECUTABLE
 #endif
 
 #ifdef _MSC_VER
