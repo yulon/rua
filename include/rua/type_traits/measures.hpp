bb361860574734f760f5b01b7677bbf563ae6ca4
Yulon <yulon@me.com>
2019-09-10 00:51:53 +0800

    Added max_*_of<...>{}

--- include/rua/type_traits/measures.hpp
+++ include/rua/type_traits/measures.hpp
@@ -22,6 +22,8 @@ template <typename T>
 inline constexpr auto size_of_v = size_of<T>::value;
 #endif
 
+////////////////////////////////////////////////////////////////////////////
+
 template <typename T>
 struct align_of {
 	static constexpr size_t value = alignof(T);
@@ -35,6 +37,52 @@ struct align_of<void> {
 #if RUA_CPP > RUA_CPP_17 || defined(__cpp_inline_variables)
 template <typename T>
 inline constexpr auto align_of_v = align_of<T>::value;
+#endif
+
+////////////////////////////////////////////////////////////////////////////
+
+template <typename... Types>
+struct max_size_of;
+
+template <typename Last>
+struct max_size_of<Last> {
+	static constexpr size_t value = size_of<Last>::value;
+};
+
+template <typename First, typename... Others>
+struct max_size_of<First, Others...> {
+	static constexpr size_t value =
+		size_of<First>::value > max_size_of<Others...>::value
+			? size_of<First>::value
+			: max_size_of<Others...>::value;
+};
+
+#if RUA_CPP > RUA_CPP_17 || defined(__cpp_inline_variables)
+template <typename... Types>
+inline constexpr auto max_size_of_v = max_size_of<Types...>::value;
+#endif
+
+////////////////////////////////////////////////////////////////////////////
+
+template <typename... Types>
+struct max_align_of;
+
+template <typename Last>
+struct max_align_of<Last> {
+	static constexpr size_t value = align_of<Last>::value;
+};
+
+template <typename First, typename... Others>
+struct max_align_of<First, Others...> {
+	static constexpr size_t value =
+		align_of<First>::value > max_align_of<Others...>::value
+			? align_of<First>::value
+			: max_align_of<Others...>::value;
+};
+
+#if RUA_CPP > RUA_CPP_17 || defined(__cpp_inline_variables)
+template <typename... Types>
+inline constexpr auto max_align_of_v = max_align_of<Types...>::value;
 #endif
 
 } // namespace rua
