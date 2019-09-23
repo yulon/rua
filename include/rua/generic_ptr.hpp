730e9457e7a092c45dc99c2b2cca906af2346b8f
Yulon <yulon@me.com>
2019-09-23 15:07:30 +0800

    Renamed any_ptr -> generic_ptr

---
+++ include/rua/generic_ptr.hpp
@@ -0,0 +1,153 @@
+#ifndef _RUA_GENERIC_PTR_HPP
+#define _RUA_GENERIC_PTR_HPP
+
+#include "macros.hpp"
+
+#include <cstddef>
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+
+namespace rua {
+
+class generic_ptr {
+public:
+	generic_ptr() = default;
+
+	RUA_FORCE_INLINE constexpr generic_ptr(std::nullptr_t) : _val(0) {}
+
+	template <typename T>
+	RUA_FORCE_INLINE generic_ptr(T *src) :
+		_val(reinterpret_cast<uintptr_t>(src)) {}
+
+	template <
+		typename T,
+		typename = typename std::enable_if<
+			std::is_member_function_pointer<T>::value>::type>
+	RUA_FORCE_INLINE generic_ptr(const T &src) :
+		generic_ptr(*reinterpret_cast<const void *const *>(&src)) {}
+
+	RUA_FORCE_INLINE explicit constexpr generic_ptr(uintptr_t val) :
+		_val(val) {}
+
+	RUA_FORCE_INLINE uintptr_t &value() {
+		return _val;
+	}
+
+	RUA_FORCE_INLINE constexpr uintptr_t value() const {
+		return _val;
+	}
+
+	RUA_FORCE_INLINE constexpr operator bool() const {
+		return _val;
+	}
+
+	template <typename T>
+	RUA_FORCE_INLINE typename std::enable_if<std::is_pointer<T>::value, T>::type
+	as() const {
+		return reinterpret_cast<T>(_val);
+	}
+
+	template <typename T>
+	RUA_FORCE_INLINE typename std::
+		enable_if<std::is_member_function_pointer<T>::value, T>::type
+		as() const {
+		return *reinterpret_cast<T *>(&_val);
+	}
+
+	template <typename T>
+	RUA_FORCE_INLINE operator T() const {
+		return as<T>();
+	}
+
+	RUA_FORCE_INLINE explicit operator uintptr_t() const {
+		return _val;
+	}
+
+	RUA_FORCE_INLINE generic_ptr &operator++() {
+		++_val;
+		return *this;
+	}
+
+	RUA_FORCE_INLINE generic_ptr operator++() const {
+		return generic_ptr(_val + 1);
+	}
+
+	RUA_FORCE_INLINE generic_ptr operator++(int) {
+		return generic_ptr(_val++);
+	}
+
+	RUA_FORCE_INLINE generic_ptr &operator--() {
+		--_val;
+		return *this;
+	}
+
+	RUA_FORCE_INLINE generic_ptr operator--() const {
+		return generic_ptr(_val - 1);
+	}
+
+	RUA_FORCE_INLINE generic_ptr operator--(int) {
+		return generic_ptr(_val--);
+	}
+
+	template <typename T>
+	RUA_FORCE_INLINE generic_ptr operator+(T &&target) const {
+		return generic_ptr(_val + static_cast<uintptr_t>(target));
+	}
+
+	RUA_FORCE_INLINE ptrdiff_t operator-(const generic_ptr &target) const {
+		return static_cast<ptrdiff_t>(_val - target._val);
+	}
+
+	template <
+		typename T,
+		typename = typename std::enable_if<!std::is_convertible<
+			typename std::decay<T>::type,
+			generic_ptr>::value>::type>
+	RUA_FORCE_INLINE generic_ptr operator-(T &&target) const {
+		return generic_ptr(_val - static_cast<uintptr_t>(target));
+	}
+
+	RUA_FORCE_INLINE bool operator>(const generic_ptr &target) const {
+		return _val > target._val;
+	}
+
+	RUA_FORCE_INLINE bool operator<(const generic_ptr &target) const {
+		return _val < target._val;
+	}
+
+	RUA_FORCE_INLINE bool operator>=(const generic_ptr &target) const {
+		return _val >= target._val;
+	}
+
+	RUA_FORCE_INLINE bool operator<=(const generic_ptr &target) const {
+		return _val <= target._val;
+	}
+
+	RUA_FORCE_INLINE bool operator==(const generic_ptr &target) const {
+		return _val == target._val;
+	}
+
+	template <typename T>
+	RUA_FORCE_INLINE generic_ptr &operator+=(T &&target) {
+		_val += static_cast<uintptr_t>(target);
+		return *this;
+	}
+
+	template <
+		typename T,
+		typename = typename std::enable_if<!std::is_convertible<
+			typename std::decay<T>::type,
+			generic_ptr>::value>::type>
+	RUA_FORCE_INLINE generic_ptr &operator-=(T &&target) {
+		_val -= static_cast<uintptr_t>(target);
+		return *this;
+	}
+
+private:
+	uintptr_t _val;
+};
+
+} // namespace rua
+
+#endif
