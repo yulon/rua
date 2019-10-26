eb7de4839dab0dda893b702976e1fef6f49aaeb5
Yulon <yulon@me.com>
2019-10-26 23:18:58 +0800

    Improved RUA_CODE_FN()

--- include/rua/code_fn.hpp
+++ include/rua/code_fn.hpp
@@ -15,30 +15,30 @@ namespace rua {
 #define _RUA_EXECUTABLE_CODE __attribute__((section(".text$_ZN3rua10text")))
 #endif
 
-#define _RUA_TO_EXECUTABLE_CODE(code) reinterpret_cast<uintptr_t>(code)
-
-#else
-
-#define _RUA_EXECUTABLE_CODE
-
-template <size_t N>
-RUA_FORCE_INLINE uintptr_t _to_executable_code(const byte (&code)[N]) {
-	mem_chmod(code, N, mem_read | mem_exec);
-	return reinterpret_cast<uintptr_t>(code);
-}
-
-#define _RUA_TO_EXECUTABLE_CODE(code) _to_executable_code(code)
-
-#endif
-
 #define RUA_CODE(name) static const byte name[] _RUA_EXECUTABLE_CODE
 
 #define RUA_CODE_FN(ret, name, params, args, code)                             \
-	RUA_FORCE_INLINE ret name params {                                         \
-		static ret(*fn_ptr) params =                                           \
-			reinterpret_cast<ret(*) params>(_RUA_TO_EXECUTABLE_CODE(code));    \
-		return fn_ptr args;                                                    \
-	}
+	static ret(*name) params = reinterpret_cast<ret(*) params>(&code[0]);
+
+#else
+
+template <typename T, size_t N>
+RUA_FORCE_INLINE T _to_executable_code(const byte (&code)[N]) {
+	mem_chmod(&code[0], N, mem_read | mem_exec);
+	return reinterpret_cast<T>(&code[0]);
+}
+
+#define RUA_CODE(name) RUA_MULTIDEF_VAR const byte name[]
+
+#define RUA_CODE_FN(ret, name, params, args, code)                             \
+	using _##name##_t = ret(*) params;                                         \
+	RUA_FORCE_INLINE _##name##_t _##name() {                                   \
+		static _##name##_t f = _to_executable_code<_##name##_t>(code);         \
+		return f;                                                              \
+	}                                                                          \
+	static _##name##_t name = _##name();
+
+#endif
 
 } // namespace rua
 
