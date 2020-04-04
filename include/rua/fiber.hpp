75c1ebfb419ed1b5ea32edb6fd7fa8bdd8cf0bf5
Yulon <yulon@me.com>
2020-04-04 10:44:58 +0800

    refactor: revert fiber{}

--- include/rua/fiber.hpp
+++ include/rua/fiber.hpp
@@ -8,6 +8,7 @@
 #include "ucontext.hpp"
 
 #include <array>
+#include <atomic>
 #include <cassert>
 #include <functional>
 #include <list>
@@ -19,35 +20,48 @@ namespace rua {
 
 class fiber_executor;
 
-class _fctx_t {
-private:
-	std::function<void()> tsk;
-
-	bool is_stoped;
-	time end_ti;
-
-	ucontext_t _uc;
-	int stk_ix;
-	bytes stk_bak;
-
-	bool has_yielded;
-	std::shared_ptr<secondary_waker> wkr;
-
-	friend fiber_executor;
-};
-
-class fid_t {
+class fiber {
 public:
-	constexpr fid_t() = default;
+	constexpr fiber() = default;
 
 	operator bool() const {
-		return _p.get();
+		return _ctx.get();
+	}
+
+	void reset_lifetime(ms dur = 0) {
+		if (!dur) {
+			_ctx->end_ti.reset();
+		}
+		auto now = tick();
+		if (dur >= time_max() - now) {
+			_ctx->end_ti = time_max();
+			return;
+		}
+		_ctx->end_ti = now + dur;
+	}
+
+	void stop() {
+		_ctx->is_stoped.store(true);
 	}
 
 private:
-	std::shared_ptr<_fctx_t> _p;
+	struct _ctx_t {
+		std::function<void()> tsk;
 
-	fid_t(std::shared_ptr<_fctx_t> p) : _p(std::move(p)) {}
+		std::atomic<bool> is_stoped;
+		time end_ti;
+
+		ucontext_t _uc;
+		int stk_ix;
+		bytes stk_bak;
+
+		bool has_yielded;
+		std::shared_ptr<secondary_waker> wkr;
+	};
+
+	std::shared_ptr<_ctx_t> _ctx;
+
+	fiber(std::shared_ptr<_ctx_t> ctx) : _ctx(std::move(ctx)) {}
 
 	friend fiber_executor;
 };
@@ -57,33 +71,17 @@ public:
 	fiber_executor(size_t stack_size = 0x100000) :
 		_stk_sz(stack_size), _stk_ix(0), _sch(*this) {}
 
-	fid_t execute(std::function<void()> func, ms lifetime = 0) {
-		fid_t ctx(std::make_shared<_fctx_t>());
-		ctx._p->tsk = std::move(func);
-		ctx._p->is_stoped = false;
-		reset_lifetime(ctx, lifetime);
-		_exs.emplace(ctx);
-		return ctx;
+	fiber execute(std::function<void()> task, ms lifetime = 0) {
+		fiber f(std::make_shared<fiber::_ctx_t>());
+		f._ctx->tsk = std::move(task);
+		f._ctx->is_stoped.store(false);
+		f.reset_lifetime(lifetime);
+		_exs.emplace(f);
+		return f;
 	}
 
-	fid_t current() const {
-		return _cur_ctx;
-	}
-
-	void reset_lifetime(fid_t ctx, ms dur = 0) {
-		if (!dur) {
-			ctx._p->end_ti.reset();
-		}
-		auto now = tick();
-		if (dur >= time_max() - now) {
-			ctx._p->end_ti = time_max();
-			return;
-		}
-		ctx._p->end_ti = now + dur;
-	}
-
-	void unexecute(fid_t ctx) {
-		ctx._p->is_stoped = true;
+	fiber executing() const {
+		return _cur;
 	}
 
 	operator bool() const {
@@ -107,7 +105,7 @@ public:
 		}
 
 		scheduler_guard sg(_sch);
-		_switch_to_worker_uc();
+		_switch_to_runner_uc();
 	}
 
 	// May block the current context.
@@ -131,7 +129,7 @@ public:
 		}
 
 		for (;;) {
-			_switch_to_worker_uc();
+			_switch_to_runner_uc();
 
 			now = tick();
 
@@ -203,19 +201,19 @@ public:
 				wake_ti = tick() + timeout;
 			}
 
-			_fe->_cur_ctx._p->has_yielded = true;
-			_fe->_cur_ctx._p->stk_ix = _fe->_stk_ix;
-			slp_map.emplace(wake_ti, _fe->_cur_ctx);
+			_fe->_cur._ctx->has_yielded = true;
+			_fe->_cur._ctx->stk_ix = _fe->_stk_ix;
+			slp_map.emplace(wake_ti, _fe->_cur);
 
-			_fe->_prev_ctx = std::move(_fe->_cur_ctx);
+			_fe->_prev = std::move(_fe->_cur);
 			if (_fe->_exs.size()) {
-				if (!_fe->_try_resume_ctxs_front()) {
-					_fe->_swap_new_worker_uc(&_fe->_prev_ctx._p->_uc);
+				if (!_fe->_try_resume_exs_front()) {
+					_fe->_swap_new_runner_uc(&_fe->_prev._ctx->_uc);
 				}
 			} else {
-				swap_ucontext(&_fe->_prev_ctx._p->_uc, &_fe->_orig_uc);
+				swap_ucontext(&_fe->_prev._ctx->_uc, &_fe->_orig_uc);
 			}
-			_fe->_clear_prev_ctx();
+			_fe->_clear_prev();
 		}
 
 		virtual bool sleep(ms timeout, bool wakeable = false) {
@@ -224,7 +222,7 @@ public:
 				return false;
 			}
 
-			auto &wkr = _fe->_cur_ctx._p->wkr;
+			auto &wkr = _fe->_cur._ctx->wkr;
 			if (!wkr->state()) {
 				_sleep(_fe->_cws, timeout);
 			}
@@ -232,9 +230,9 @@ public:
 		}
 
 		virtual waker_i get_waker() {
-			assert(_fe->_cur_ctx);
+			assert(_fe->_cur);
 
-			auto &wkr = _fe->_cur_ctx._p->wkr;
+			auto &wkr = _fe->_cur._ctx->wkr;
 			if (wkr) {
 				wkr.reset();
 			} else {
@@ -256,11 +254,12 @@ public:
 	}
 
 private:
-	std::queue<fid_t> _exs;
-	fid_t _cur_ctx, _prev_ctx;
+	std::queue<fiber> _exs;
+	fiber _cur, _prev;
 
-	std::multimap<time, fid_t> _slps;
-	std::multimap<time, fid_t> _cws;
+	// TODO: time wheel
+	std::multimap<time, fiber> _slps;
+	std::multimap<time, fiber> _cws;
 
 	void _check_slps(time now) {
 		for (auto it = _slps.begin(); it != _slps.end(); it = _slps.erase(it)) {
@@ -273,7 +272,7 @@ private:
 
 	void _check_cws(time now) {
 		for (auto it = _cws.begin(); it != _cws.end();) {
-			if (it->second._p->wkr->state() || now >= it->first) {
+			if (it->second._ctx->wkr->state() || now >= it->first) {
 				_exs.emplace(std::move(it->second));
 				it = _cws.erase(it);
 				continue;
@@ -287,39 +286,38 @@ private:
 	size_t _stk_sz;
 	int _stk_ix;
 	bytes _stks[2];
-	ucontext_t _new_worker_ucs[2];
+	ucontext_t _new_runner_ucs[2];
 
 	bytes &_cur_stk() {
 		return _stks[_stk_ix];
 	}
 
-	ucontext_t &_cur_new_worker_uc() {
-		return _new_worker_ucs[_stk_ix];
+	ucontext_t &_cur_new_runner_uc() {
+		return _new_runner_ucs[_stk_ix];
 	}
 
-	void _clear_prev_ctx() {
-		if (!_prev_ctx) {
+	void _clear_prev() {
+		if (!_prev) {
 			return;
 		}
 
-		assert(!_prev_ctx._p->stk_bak.size());
+		assert(!_prev._ctx->stk_bak.size());
 
-		auto &stk = _stks[_prev_ctx._p->stk_ix];
-		auto stk_used = stk(_prev_ctx._p->_uc.sp() - stk.data().uintptr());
+		auto &stk = _stks[_prev._ctx->stk_ix];
+		auto stk_used = stk(_prev._ctx->_uc.sp() - stk.data().uintptr());
 		auto rmdr = stk_used.size() % 1024;
-		_prev_ctx._p->stk_bak.resize(
-			stk_used.size() + (rmdr ? 1024 - rmdr : 0));
-		_prev_ctx._p->stk_bak = stk_used;
+		_prev._ctx->stk_bak.resize(stk_used.size() + (rmdr ? 1024 - rmdr : 0));
+		_prev._ctx->stk_bak = stk_used;
 
-		_prev_ctx._p.reset();
+		_prev._ctx.reset();
 	}
 
-	bool _try_resume_ctxs_front(ucontext_t *oucp = nullptr) {
-		if (!_exs.front()._p->stk_bak.size()) {
+	bool _try_resume_exs_front(ucontext_t *oucp = nullptr) {
+		if (!_exs.front()._ctx->stk_bak.size()) {
 			return false;
 		}
 
-		if (oucp != &_orig_uc && _exs.front()._p->stk_ix == _stk_ix) {
+		if (oucp != &_orig_uc && _exs.front()._ctx->stk_ix == _stk_ix) {
 			if (!oucp) {
 				set_ucontext(&_orig_uc);
 				return true;
@@ -328,84 +326,84 @@ private:
 			return true;
 		}
 
-		_cur_ctx = std::move(_exs.front());
+		_cur = std::move(_exs.front());
 		_exs.pop();
 
-		_stk_ix = _cur_ctx._p->stk_ix;
+		_stk_ix = _cur._ctx->stk_ix;
 		auto &cur_stk = _cur_stk();
-		cur_stk(cur_stk.size() - _cur_ctx._p->stk_bak.size())
-			.copy_from(_cur_ctx._p->stk_bak);
-		_cur_ctx._p->stk_bak.resize(0);
+		cur_stk(cur_stk.size() - _cur._ctx->stk_bak.size())
+			.copy_from(_cur._ctx->stk_bak);
+		_cur._ctx->stk_bak.resize(0);
 
 		if (!oucp) {
-			set_ucontext(&_cur_ctx._p->_uc);
+			set_ucontext(&_cur._ctx->_uc);
 			return true;
 		}
-		swap_ucontext(oucp, &_cur_ctx._p->_uc);
+		swap_ucontext(oucp, &_cur._ctx->_uc);
 		return true;
 	}
 
-	void _work() {
-		_clear_prev_ctx();
+	void _run() {
+		_clear_prev();
 
 		while (_exs.size()) {
 			assert(_exs.front());
-			_try_resume_ctxs_front();
+			_try_resume_exs_front();
 
-			_cur_ctx = std::move(_exs.front());
+			_cur = std::move(_exs.front());
 			assert(!_exs.front());
 			_exs.pop();
 
-			if (_cur_ctx._p->is_stoped) {
+			if (_cur._ctx->is_stoped.load()) {
 				continue;
 			}
 
 			for (;;) {
-				_cur_ctx._p->tsk();
+				_cur._ctx->tsk();
 
-				if (_cur_ctx._p->is_stoped) {
+				if (_cur._ctx->is_stoped.load()) {
 					break;
 				}
 
-				if (_cur_ctx._p->end_ti <= tick()) {
-					_cur_ctx._p->is_stoped = false;
+				if (_cur._ctx->end_ti <= tick()) {
+					_cur._ctx->is_stoped.store(false);
 					break;
 				}
 
-				if (!_cur_ctx._p->has_yielded) {
-					_slps.emplace(time_zero(), std::move(_cur_ctx));
+				if (!_cur._ctx->has_yielded) {
+					_slps.emplace(time_zero(), std::move(_cur));
 					break;
 				}
-				_cur_ctx._p->has_yielded = false;
+				_cur._ctx->has_yielded = false;
 			}
 		}
 
 		set_ucontext(&_orig_uc);
 	}
 
-	static void _worker(any_word th1s) {
-		th1s.as<fiber_executor *>()->_work();
+	static void _runner(any_word th1s) {
+		th1s.as<fiber_executor *>()->_run();
 	}
 
-	void _swap_new_worker_uc(ucontext_t *oucp) {
+	void _swap_new_runner_uc(ucontext_t *oucp) {
 		if (oucp != &_orig_uc) {
 			_stk_ix = !_stk_ix;
 		}
 		auto &cur_stk = _cur_stk();
 		if (!cur_stk) {
 			cur_stk.reset(_stk_sz);
-			get_ucontext(&_cur_new_worker_uc());
-			make_ucontext(&_cur_new_worker_uc(), &_worker, this, cur_stk);
+			get_ucontext(&_cur_new_runner_uc());
+			make_ucontext(&_cur_new_runner_uc(), &_runner, this, cur_stk);
 		}
-		swap_ucontext(oucp, &_cur_new_worker_uc());
+		swap_ucontext(oucp, &_cur_new_runner_uc());
 	}
 
-	void _switch_to_worker_uc() {
+	void _switch_to_runner_uc() {
 		while (_exs.size()) {
-			if (!_try_resume_ctxs_front(&_orig_uc)) {
-				_swap_new_worker_uc(&_orig_uc);
+			if (!_try_resume_exs_front(&_orig_uc)) {
+				_swap_new_runner_uc(&_orig_uc);
 			}
-			_clear_prev_ctx();
+			_clear_prev();
 		}
 	}
 
@@ -415,18 +413,35 @@ private:
 	waker_i _orig_wkr;
 };
 
-inline fid_t co(std::function<void()> func, ms lifetime = 0) {
+inline fiber_executor *this_fiber_executor() {
 	auto sch = this_scheduler();
-	if (sch) {
-		auto fib_sch = sch.as<fiber_executor::scheduler>();
-		if (fib_sch) {
-			return fib_sch->get_executor().execute(std::move(func), lifetime);
-		}
+	if (!sch) {
+		return nullptr;
 	}
-	auto fib_exr = std::make_shared<fiber_executor>();
-	fib_exr->execute(std::move(func), lifetime);
-	fib_exr->run();
-	return fid_t();
+	auto fs = sch.as<fiber_executor::scheduler>();
+	if (!fs) {
+		return nullptr;
+	}
+	return &fs->get_executor();
+}
+
+inline fiber this_fiber() {
+	auto fe = this_fiber_executor();
+	if (fe) {
+		return fe->executing();
+	}
+	return fiber();
+}
+
+inline fiber co(std::function<void()> task, ms lifetime = 0) {
+	auto fe = this_fiber_executor();
+	if (fe) {
+		return fe->execute(std::move(task), lifetime);
+	}
+	auto tmp_fe = std::make_shared<fiber_executor>();
+	tmp_fe->execute(std::move(task), lifetime);
+	tmp_fe->run();
+	return fiber();
 }
 
 } // namespace rua
