ae816dc6728b2a7c17a2542bc264d082383c42b3
Yulon <yulon@me.com>
2021-12-10 06:23:15 +0800

    refactor(win32): remove named pipe

--- include/rua/pipe.hpp
+++ include/rua/pipe.hpp
@@ -1,87 +1,22 @@
 #ifndef _RUA_PIPE_HPP
 #define _RUA_PIPE_HPP
 
-#include "string.hpp"
-#include "sys/stream.hpp"
-#include "types/util.hpp"
+#include "macros.hpp"
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
+#if defined(_WIN32)
 
-#include <cassert>
-#include <cstring>
+#include "pipe/win32.hpp"
 
 namespace rua {
 
-struct pipe_accessors {
-	sys_stream r, w;
-
-	operator bool() const {
-		return r || w;
-	}
-};
-
-#if defined(_WIN32)
-inline bool make_pipe(sys_stream &r, sys_stream &w, bool inherit = true) {
-	SECURITY_ATTRIBUTES sa;
-	memset(&sa, 0, sizeof(SECURITY_ATTRIBUTES));
-	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
-	sa.bInheritHandle = inherit;
-
-	HANDLE rh, wh;
-	if (!CreatePipe(&rh, &wh, &sa, 0)) {
-		return false;
-	}
-	r = rh;
-	w = wh;
-	return true;
-}
-
-inline pipe_accessors make_pipe(bool inherit = true) {
-	pipe_accessors pa;
-	make_pipe(pa.r, pa.w, inherit);
-	return pa;
-}
-
-inline sys_stream make_pipe(string_view name, size_t timeout = nmax<size_t>()) {
-	auto h = CreateNamedPipeW(
-		u8_to_w(str_join("\\\\.\\pipe\\", name)).data(),
-		PIPE_ACCESS_INBOUND | PIPE_ACCESS_OUTBOUND,
-		PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
-		PIPE_UNLIMITED_INSTANCES,
-		0,
-		0,
-		static_cast<DWORD>(timeout),
-		0);
-	if (!h) {
-		return nullptr;
-	}
-	if (!ConnectNamedPipe(h, nullptr)) {
-		return nullptr;
-	}
-	return h;
-}
-
-inline sys_stream open_pipe(string_view name, size_t timeout = nmax<size_t>()) {
-	auto fmt_name = u8_to_w(str_join("\\\\.\\pipe\\", name));
-
-	if (!WaitNamedPipeW(fmt_name.c_str(), static_cast<DWORD>(timeout))) {
-		return nullptr;
-	}
-	return CreateFileW(
-		fmt_name.c_str(),
-		GENERIC_READ | GENERIC_WRITE,
-		0,
-		nullptr,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		nullptr);
-}
-#else
-#error rua::pipe: not supported this platform!
-#endif
+using namespace win32::_pipe;
 
 } // namespace rua
+
+#else
+
+#error rua::pipe: not supported this platform!
+
+#endif
 
 #endif
---
+++ include/rua/pipe/win32.hpp
@@ -0,0 +1,51 @@
+#ifndef _RUA_PIPE_WIN32_HPP
+#define _RUA_PIPE_WIN32_HPP
+
+#include "../chrono/duration.hpp"
+#include "../string.hpp"
+#include "../sys/stream/win32.hpp"
+#include "../types/util.hpp"
+
+#include <windows.h>
+
+#include <cassert>
+
+namespace rua { namespace win32 {
+
+namespace _pipe {
+
+struct pipe {
+	sys_stream reader, writer;
+
+	operator bool() const {
+		return reader || writer;
+	}
+};
+
+inline bool make_pipe(sys_stream &reader, sys_stream &writer) {
+	SECURITY_ATTRIBUTES sa;
+	memset(&sa, 0, sizeof(SECURITY_ATTRIBUTES));
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+
+	HANDLE rh, wh;
+	if (!CreatePipe(&rh, &wh, &sa, 0)) {
+		return false;
+	}
+	reader = rh;
+	writer = wh;
+	return true;
+}
+
+inline pipe make_pipe() {
+	pipe pa;
+	make_pipe(pa.reader, pa.writer);
+	return pa;
+}
+
+} // namespace _pipe
+
+using namespace _pipe;
+
+}} // namespace rua::win32
+
+#endif
