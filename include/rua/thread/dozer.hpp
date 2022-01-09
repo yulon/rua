e94d4f7c000cbc96bea55415fd4fe532e6135c9c
Yulon <yulon@me.com>
2022-01-09 14:50:21 +0800

    refactor: remove darwin::thread_dozer{}
    stop using GCD semaphore

--- include/rua/thread/dozer.hpp
+++ include/rua/thread/dozer.hpp
@@ -13,16 +13,6 @@ using thread_dozer = win32::thread_dozer;
 
 } // namespace rua
 
-#elif defined(RUA_DARWIN)
-
-#include "dozer/darwin.hpp"
-
-namespace rua {
-
-using thread_dozer = darwin::thread_dozer;
-
-} // namespace rua
-
 #elif defined(RUA_UNIX) || RUA_HAS_INC(<pthread.h>) || defined(_PTHREAD_H)
 
 #include "dozer/posix.hpp"
