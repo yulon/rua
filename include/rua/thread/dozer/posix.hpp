03ddfa718849884c3365efd53e11cbb0a54cf300
Yulon <yulon@me.com>
2022-01-18 05:02:11 +0800

    fix(posix): avoid thread_waker::wake() waking thread_dozer::doze() early

--- include/rua/thread/dozer/posix.hpp
+++ include/rua/thread/dozer/posix.hpp
@@ -15,6 +15,7 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <atomic>
 #include <cassert>
 #include <memory>
 
@@ -32,11 +33,38 @@ public:
 	}
 
 	virtual void wake() {
-		pthread_kill(_tid, SIGCONT);
+		auto old_state = _state.exchange(2);
+		if (!old_state || old_state == 2) {
+			return;
+		}
+		while (!pthread_kill(_tid, SIGCONT) && _state.load())
+			;
+	}
+
+	void reset() {
+		_state.store(0);
+	}
+
+	bool try_doze() {
+		auto old_state = _state.load();
+		if (old_state == 2) {
+			reset();
+			return false;
+		}
+		assert(!old_state);
+		while (!_state.compare_exchange_weak(old_state, 1)) {
+			assert(old_state != 1);
+			if (old_state == 2) {
+				reset();
+				return false;
+			}
+		}
+		return true;
 	}
 
 private:
 	pthread_t _tid;
+	std::atomic<uintptr_t> _state;
 };
 
 class thread_dozer : public dozer_base {
@@ -77,12 +105,21 @@ public:
 	virtual bool doze(duration timeout = duration_max()) {
 		assert(_wkr);
 
+		if (!_wkr->try_doze()) {
+			return true;
+		}
 		auto c_ti = timeout.c_timespec();
-		return ::nanosleep(&c_ti, nullptr) == -1;
+		auto is_woke = ::nanosleep(&c_ti, nullptr) == -1;
+		if (is_woke) {
+			_wkr->reset();
+		}
+		return is_woke;
 	}
 
 	virtual waker_i get_waker() {
-		if (!_wkr) {
+		if (_wkr) {
+			_wkr->reset();
+		} else {
 			static auto act_r = ([]() -> int {
 				static struct sigaction new_act, old_act;
 				new_act.sa_handler = [](int sig) {
